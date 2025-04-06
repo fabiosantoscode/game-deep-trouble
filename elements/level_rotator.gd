3e06fabbfd722e30abe9b63256eedcd1b0f29082
Fábio Santos <fabiosantosart@gmail.com>
2025-04-06 15:25:44 +0100

    delete level 11

--- elements/level_rotator.gd
+++ elements/level_rotator.gd
@@ -8,7 +8,7 @@ const LEVEL_END_SCREEN = preload("res://levels/level_end_screen.tscn")
 var level_scene_name_start = "res://levels/level"
 var level_scene_name_end = ".tscn"
 var current_level = 0
-var level_count = 11
+var level_count = 10
 
 static func find_level_rotator(from_child: Node) -> LevelRotator:
 	return from_child.find_parent("LevelRotator")
