09a7f8abdec70210c93e6fc5c66c9af88c1258d9
Fábio Santos <fabiosantosart@gmail.com>
2025-09-04 10:58:14 +0100

    change level rotation strategy to an array of levels

--- elements/level_rotator.gd
+++ elements/level_rotator.gd
@@ -7,45 +7,48 @@ const LEVEL_END_SCREEN = preload("res://levels/level_end_screen.tscn")
 const ON_DEATH = preload("res://elements/on_death.tscn")
 const ON_LEVEL_COMPLETE = preload("res://elements/on_level_complete.tscn")
 
-var level_scene_name_start = "res://levels/level"
-var level_scene_name_end = ".tscn"
+## level files are "res://levels/level" {any digit} {anything} ".tscn"
+static var all_level_files = _find_all_levels()
+## Index into all_level_files
 var current_level = 0
-var level_count = 10
-
-static func find_level_rotator(from_child: Node) -> LevelRotator:
-	return from_child.find_parent("LevelRotator")
 
 static func restart_level(from_child: Node):
-	var level_rot = find_level_rotator(from_child)
+	var level_rot = _find_level_rotator(from_child)
 	if level_rot != null:
 		level_rot._low_level_set_level(ON_DEATH)
-		await level_rot.get_tree().create_timer(0.5).timeout
+		await level_rot.get_tree().create_timer(0.75).timeout
 		level_rot._start_level(level_rot.current_level)
 
 static func title_screen(from_child: Node):
-	var level_rot = find_level_rotator(from_child)
+	var level_rot = _find_level_rotator(from_child)
 	print("Starting level ", "_start_title_screen")
 	if level_rot != null:
 		level_rot._start_title_screen()
 
 
 static func next_level(from_child: Node):
-	var level_rot = find_level_rotator(from_child)
+	var level_rot = _find_level_rotator(from_child)
 	if level_rot != null:
+		# Jingle does not apply to level 0 which is the lore dump
 		if level_rot.current_level > 0:
 			level_rot._low_level_set_level(ON_LEVEL_COMPLETE)
 			await level_rot.get_tree().create_timer(1.5).timeout
-		if level_rot.current_level == level_rot.level_count:
+
+		if level_rot.current_level >= len(all_level_files):
 			print("Starting level ", "_beat_game")
 			level_rot._beat_game.call_deferred()
 		else:
-			print("Starting level ", level_rot.current_level + 1)
-			level_rot._start_level.call_deferred(level_rot.current_level + 1)
+			level_rot.current_level += 1
+			print("Starting level ", all_level_files[level_rot.current_level])
+			level_rot._start_level.call_deferred(level_rot.current_level)
 
 static func story_level(from_child: Node):
-	var level_rot = find_level_rotator(from_child)
+	var level_rot = _find_level_rotator(from_child)
 	if level_rot != null:
 		level_rot._start_level(0)
+
+static func _find_level_rotator(from_child: Node) -> LevelRotator:
+	return from_child.find_parent("LevelRotator")
 
 func _ready():
 	_start_title_screen()
@@ -63,13 +66,8 @@ func _clear_current_level():
 func _start_level(level_num: int):
 	_clear_current_level()
 
-	current_level = level_num
-
-	var level_file = level_scene_name_start + str(level_num) + level_scene_name_end
-	var level = load(level_file).instantiate()
+	var level = load(all_level_files[level_num]).instantiate()
 	level_container.add_child(level)
-	
-	get_window().title = "Deep Trouble - level " + str(level_num)
 
 func _start_title_screen():
 	_low_level_set_level(LEVEL_TITLE_SCREEN)
@@ -80,3 +78,12 @@ func _beat_game():
 func _low_level_set_level(packed_scene):
 	_clear_current_level()
 	level_container.add_child(packed_scene.instantiate())
+
+static func _find_all_levels():
+	var ret = []
+	for file in DirAccess.open("res://levels/").get_files():
+		if file.begins_with("level") and file.ends_with(".tscn"):
+			var digit = file[len("level")]
+			if "0123456789".contains(digit):
+				ret.push_back("res://levels/" + file)
+	return ret
