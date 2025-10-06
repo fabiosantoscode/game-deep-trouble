0e99dee6a3a8a48826ce8819801fb3ad239a90c5
Fábio Santos <fabiosantosart@gmail.com>
2025-10-06 16:16:08 +0100

    use aseprite files not png

--- elements/goal.gd
+++ elements/goal.gd
@@ -3,8 +3,10 @@ class_name Goal
 
 @onready var area_2d: Area2D = $Area2D
 @export var requires_rock = true
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 func _ready() -> void:
+	animated_sprite_2d.play("default")
 	area_2d.body_entered.connect(_on_sub)
 
 func _on_sub(sub: Sub):
